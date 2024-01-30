1a147c866430cc71a84b7872e20ea67b98d7cb73
아르페 <ore@nrma.cc>
2024-01-31 03:05:30 +0900

    fix: unchanged locale

--- src/frontend/langs/index.ts
+++ src/frontend/langs/index.ts
@@ -1,10 +1,6 @@
 import jaJP from './ja-JP.json';
 import enUS from './en-US.json';
 import koKR from './ko-KR.json';
-import jaCR from './ja-cr.json';
-import itIT from './it-IT.json';
-import zhCN from './zh-CN.json';
-import zhTW from './zh-TW.json';
 
 import deepmerge from 'deepmerge';
 
@@ -15,30 +11,23 @@ const merge = (baseData: Record<string, unknown>, newData: Record<string, unknow
 };
 
 const _enUS = merge(jaJP, enUS);
+const _koKR = merge(_enUS, koKR)
 
 export const resources = {
   'ja_JP': { translation: jaJP },
   'en_US': { translation: _enUS },
-  'ko_KR': { translation: merge(_enUS, koKR) },
-  'it_IT': { translation: merge(_enUS, itIT) },
-  'zh_CN': { translation: merge(_enUS, zhCN) },
-  'zh_TW': { translation: merge(_enUS, zhTW) },
-  'ja_CR': { translation: merge(jaJP, jaCR) },
+  'ko_KR': { translation: _koKR },
 };
 
 export const languageName = {
   'ja_JP': '日本語',
   'en_US': 'English',
   'ko_KR': '한국어',
-  'it_IT': 'Italiano',
-  'zh_CN': '简体中文',
-  'zh_TW': '繁體中文',
-  'ja_CR': '怪レい日本语',
 } as const;
 
 export type LanguageCode = keyof typeof resources;
 
 export const getBrowserLanguage = () => {
   const lang = navigator.language.replace('-', '_').toLowerCase();
-  return (Object.keys(resources) as LanguageCode[]).find(k => k.toLowerCase().startsWith(lang)) ?? 'en_US';
+  return (Object.keys(resources) as LanguageCode[]).find(k => k.toLowerCase().startsWith(lang)) ?? 'ko_KR';
 };
